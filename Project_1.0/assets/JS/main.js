ca817a316d124870a3afb8f9da81f2be69fcc092
SATODHRU NONDON <66862360+shoto2020@users.noreply.github.com>
2021-02-11 17:46:39 +0600

    first commit

---
+++ Project_1.0/assets/JS/main.js
@@ -0,0 +1,14 @@
+$(document).ready ( function () {
+
+    $('.js-wp-2').waypoint(function(direction) {
+
+        $('.js-wp-2').addclass('animate slideInUp');
+
+    }, {
+
+        offset: '70%'
+
+    });
+
+});
+
